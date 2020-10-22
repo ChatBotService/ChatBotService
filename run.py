770f0c4bec49b55772f0304d3ece70326bb23b05
JureBevc <bevc.jure@gmail.com>
2020-10-22 03:05:22 +0200

    Basis for API implementation. Static file seriving.

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
-from chatbot_test import service_test
-
+from flask import Flask, render_template
+from flask_restful import Api
+from api.rest_test import TestResource
 print("Running...")
-service_test.run()
+
+app = Flask(__name__)
+api = Api(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+app.config["DEBUG"] = True
+
+api.add_resource(TestResource,"/test", "/test/<int:id>")
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+app.run(host="0.0.0.0", port=80, debug=True)
